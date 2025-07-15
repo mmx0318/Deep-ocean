0c4ac16f913eddc31193682d7ec81bb6966ad936
mmx0318 <113878538+mmx0318@users.noreply.github.com>
2025-07-15 23:45:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const CONFIG = {
-    KIMI_BASE_URL: window.location.origin + '/api/kimi',
+    //KIMI_BASE_URL: window.location.origin + '/api/kimi',
+    KIMI_BASE_URL: 'http://localhost:3001/api/kimi',
     KIMI_MODEL: 'moonshot-v1-32k',
     GENERATION_CONFIG: {
         temperature: 0.8,
